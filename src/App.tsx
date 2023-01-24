91a6bd56c8ce94c38de1cc07f34f8b23da6a9862
Thu Giang <91854884+memopussle@users.noreply.github.com>
2023-01-24 22:10:46 +1300

    Revert "install eslint-prettier"

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
 import React from "react";
-import { Heading, Stack } from "@chakra-ui/react";
 
-function App(): JSX.Element {
-  return (
-    <Stack spacing={6}>
-      <Heading as="h1" size="4xl" noOfLines={1}>
-        (4xl) In love with React & Next
-      </Heading>
-    </Stack>
-  );
+function App (): JSX.Element {
+  return <h1>Hello World</h1>;
 }
 export default App;
