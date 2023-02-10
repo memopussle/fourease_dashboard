6d3e6719a4e162b1f74f9dbbfe2869e6f08ea1e9
thugiang <xanhtham.cuc@gmail.com>
2023-02-10 22:55:02 +1300

    changing line chart style

--- src/components/lineChart/LineChart.tsx
+++ src/components/lineChart/LineChart.tsx
@@ -1,88 +1,78 @@
-
 import { ResponsiveLine } from "@nivo/line";
 import { lineData } from "../../data/lineData";
 import React, { useContext } from "react";
-import { ModeContext } from "../../useMode";
+import { ModeContext, tokens } from "../../useMode";
 
 const LineChart = (): JSX.Element => {
-const { mode } = useContext(ModeContext);
-    return (
-      <ResponsiveLine
-        data={lineData}
-        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-        xScale={{ type: "point" }}
-        yScale={{
-          type: "linear",
-          min: "auto",
-          max: "auto",
-          stacked: true,
-          reverse: false,
-        }}
-        yFormat=" >-.2f"
-        curve="basis"
-        axisTop={null}
-        axisRight={null}
-        axisBottom={
-          {
-            orient: "bottom",
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legendOffset: 36,
-            legendPosition: "middle",
-          } as any
-        }
-        axisLeft={
-          {
-            orient: "left",
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: "score",
-            legendOffset: -40,
-            legendPosition: "middle",
-          } as any
-        }
-        enableGridX={false}
-        enableGridY={false}
-        colors={{ scheme: "nivo" }}
-        lineWidth={0}
-        enablePoints={false}
-        pointSize={10}
-        pointColor={{ theme: "background" }}
-        pointBorderWidth={2}
-        pointBorderColor={{ from: "serieColor" }}
-        pointLabelYOffset={-12}
-        enableArea={true}
-        areaBlendMode={mode === "dark" ? "hard-light": "normal"}
-        areaOpacity={1}
-        useMesh={true}
-        legends={[
-          {
-            anchor: "bottom-right",
-            direction: "column",
-            justify: false,
-            translateX: 100,
-            translateY: 0,
-            itemsSpacing: 0,
-            itemDirection: "left-to-right",
-            itemWidth: 80,
-            itemHeight: 20,
-            itemOpacity: 0.75,
-            symbolSize: 12,
-            symbolShape: "circle",
-            symbolBorderColor: "rgba(0, 0, 0, .5)",
-            effects: [
-              {
-                on: "hover",
-                style: {
-                  itemBackground: "rgba(0, 0, 0, .03)",
-                  itemOpacity: 1,
-                },
-              },
-            ],
-          },
-        ]}
-      />
-    );
+  const { mode } = useContext(ModeContext);
+  const { colors } = tokens(mode);
+
+  const gradProps = {
+    gradientUnits: "userSpaceOnUse",
+    x1: "0",
+    y1: "0",
+    x2: "0",
+    y2: 400,
+  };
+
+  return (
+    <>
+      <svg style={{ width: 0, height: 0 }}>
+        <defs>
+          <linearGradient id="someGradientId" {...gradProps}>
+            <stop offset={"30%"} stopColor={colors.orangeAccent[100]} />
+            <stop offset={"20%"} stopColor={colors.orangeAccent[100]} />
+            <stop offset="100%" stopColor={colors.whiteAccent[100]} />
+          </linearGradient>
+        </defs>
+      </svg>
+      <div style={{ width: "100%", height: "100%" }}>
+        <ResponsiveLine
+          data={lineData}
+          colors={["url(#someGradientId)"]}
+          margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+          xScale={{ type: "point" }}
+          yScale={{
+            type: "linear",
+            min: "auto",
+            max: "auto",
+            stacked: true,
+            reverse: false,
+          }}
+          yFormat=" >-.2f"
+          axisTop={null}
+          axisRight={null}
+          enableArea={true}
+          enableGridX={false}
+          enablePoints={false}
+          lineWidth={1.5}
+          enableGridY={false}
+          curve="basis"
+          axisBottom={
+            {
+              orient: "bottom",
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legendOffset: 36,
+              legendPosition: "middle",
+            } as any
+          }
+          axisLeft={
+            {
+              orient: "left",
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: "score",
+              legendOffset: -40,
+              legendPosition: "middle",
+            } as any
+          }
+        />
+      </div>
+    </>
+  );
 };
+
+export default LineChart;
